211fe5b38ee5179c9fc632ab549929adb0ff13c8
Ilshat7777 <49232076+Ilshat7777@users.noreply.github.com>
2019-04-04 13:42:34 +0500

    Update kalcTest4.java

--- src/calcDepozit/kalcTest4.java
+++ src/calcDepozit/kalcTest4.java
@@ -9,8 +9,8 @@ public class kalcTest4 {
 
 	@Test
 	public void ValidTest() {
-		int a = (int)Math.round(kalc.calcSumProc(30, 30, (double)30.0/100));
-		int b =(int) Math.round(53);
+		int a = (int)Math.round(kalc.calcSumProc(205000, 48, (double)15.0/100));
+		int b =(int) Math.round(328000);
 		
 		Assert.assertEquals(a,b);
 	
